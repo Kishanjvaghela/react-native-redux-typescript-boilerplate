df77c2d95ee3d125d64f26d615531567c65ea5df
Kishan Vaghela <kisva@orsted.dk>
2019-11-26 15:55:45 +0800

    added structure from https://github.com/microsoft/TypeScript-React-Native-Starter

---
+++ src/components/atoms/Hello/styles.ts
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+// styles
+export default StyleSheet.create({
+    root: {
+        alignItems: 'center',
+        alignSelf: 'center',
+    },
+    buttons: {
+        flexDirection: 'row',
+        minHeight: 70,
+        alignItems: 'stretch',
+        alignSelf: 'center',
+        borderWidth: 5,
+    },
+    button: {
+        flex: 1,
+        paddingVertical: 0,
+    },
+    greeting: {
+        color: '#999',
+        fontWeight: 'bold',
+    },
+});
